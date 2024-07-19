99f0af50841b16adf5af6548515f0c800a94ca98
SfundoMhlungu <devadress21@gmail.com>
2024-07-20 00:51:32 +0200

    mvp with syntax highlight complete

--- content.js
+++ content.js
@@ -1,11 +1,55 @@
 // import "https://cdn.jsdelivr.net/npm/markdown-it@14.1.0/dist/markdown-it.min.js"
 // console.log(markdownIt)
 import markdownIt from './markdown-it.min.js'
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+import python from 'highlight.js/lib/languages/python';
+import C from 'highlight.js/lib/languages/c.js';
+import 'highlight.js/styles/dark.min.css';
 
+
+
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('python', python);
+hljs.registerLanguage('c', C);
+// import "./Highlight/languages/javascript.js"
+// import "./Highlight/styles/dark.min.css"
 console.log(markdownIt)
 
+function loadCSS(url) {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = url;
+    document.head.appendChild(link);
+  }
+  
+  loadCSS(chrome.runtime.getURL('./Highlight/styles/dark.min.css'));
+  
 
-const md = markdownIt();
+// hljs.registerLanguage('javascript', javascript);
+// hljs.registerLanguage('python', python);
+
+
+const styles = `
+
+pre code.hljs{display:block;overflow-x:auto;padding:1em}code.hljs{padding:3px 5px}
+.hljs{color:#c9d1d9;background:#0d1117}.hljs-doctag,.hljs-keyword,.hljs-meta .hljs-keyword,.hljs-template-tag,.hljs-template-variable,.hljs-type,.hljs-variable.language_{color:#ff7b72}.hljs-title,.hljs-title.class_,.hljs-title.class_.inherited__,.hljs-title.function_{color:#d2a8ff}.hljs-attr,.hljs-attribute,.hljs-literal,.hljs-meta,.hljs-number,.hljs-operator,.hljs-selector-attr,.hljs-selector-class,.hljs-selector-id,.hljs-variable{color:#79c0ff}.hljs-meta .hljs-string,.hljs-regexp,.hljs-string{color:#a5d6ff}.hljs-built_in,.hljs-symbol{color:#ffa657}.hljs-code,.hljs-comment,.hljs-formula{color:#8b949e}.hljs-name,.hljs-quote,.hljs-selector-pseudo,.hljs-selector-tag{color:#7ee787}.hljs-subst{color:#c9d1d9}.hljs-section{color:#1f6feb;font-weight:700}.hljs-bullet{color:#f2cc60}.hljs-emphasis{color:#c9d1d9;font-style:italic}.hljs-strong{color:#c9d1d9;font-weight:700}.hljs-addition{color:#aff5b4;background-color:#033a16}.hljs-deletion{color:#ffdcd7;background-color:#67060c}
+
+`
+
+const md = markdownIt({
+  highlight: function (str, lang) {
+    console.log("highlight js", lang);
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return `<pre class="hljs"><code class="hljs">${hljs.highlight(str, { language: lang }).value}</code></pre>`;
+      } catch (__) {}
+    }
+    return ''; // use external default escaping
+  }
+});
+
 const result = md.render('# markdown-it rulezz!');
 console.log(result, "MARKDOWN RESULT")
 // Wait for the DOM to load
@@ -72,8 +116,9 @@ console.log("content script")
                 console.log("adding white space")
                 textBlob += '\n';
             }
-            textBlob += '\n'
+           
             })
+             textBlob += '\n'
           })
           // console.log(paragraphs, "paragraphs")
           // for (let p of paragraphs) {
@@ -91,8 +136,12 @@ console.log("content script")
       
           // Create a new div with the parsed content
           const newDiv = document.createElement('div');
+          const css = document.createElement("style")
+          css.type = 'text/css';
+          css.innerHTML = styles
+          shadowRoot.appendChild(css)
           newDiv.innerHTML = parsedMarkdown;
-      
+          
           // Replace the old div with the new one
           targetDiv.parentNode.replaceChild(newDiv, targetDiv);
         }
